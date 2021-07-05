b594d4f65b14f342fd7d0a525294c5159a8d8bd3
El <547457457545678@mail.ru>
2021-07-05 14:52:37 +0700

    Задание: независимость контента, ищем баг

--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,8 @@
 #добавление в корзину со страницы товара
 import time
 
+import pytest
+
 from pages.main_page import MainPage
 from pages.product_page import ProductPage
 from pages.login_page import LoginPage
@@ -9,6 +11,31 @@ from pages.login_page import LoginPage
 def test_guest_can_add_product_to_basket(browser):
     #link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_baskets()
+    page.solve_quiz_and_get_code()
+    time.sleep(1)
+    page.item_added_to_cart(page.return_book_name())
+    page.basket_item_prices_is_correct(page.return_book_price())
+
+
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9",
+                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                                               marks=pytest.mark.xfail)])
+# @pytest.mark.parametrize('promo_offer', ["0","1", "3", "4", "5", "6", "7", "8", "9"])
+# def test_guest_can_add_product_to_basket(browser, promo_offer):
+#     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
+
+def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.add_to_baskets()
